1d503675fcfcac523e56d5e0f12e2a24aa67511a
Alexis Herrera Vegas <alexishvegas@gmail.com>
2017-08-19 12:04:51 -0300

    Boolean fun!

--- DaProject/ViewController.swift
+++ DaProject/ViewController.swift
@@ -18,7 +18,17 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any)
     {
+        let addition = true
+        
+        if addition
+        {
         theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        }
+        else
+        {
+            theLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+
+        }
     }
     
     
